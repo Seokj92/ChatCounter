938950c23fb754cbd26d2e24e0fd9ad00d6c397a
Lee Jung Seok <21201072@handong.edu>
2018-05-19 19:15:42 +0900

    commit revised one

--- src/main/java/edu/handong/csee/java/hw3/ChatMessageCounter.java
+++ src/main/java/edu/handong/csee/java/hw3/ChatMessageCounter.java
@@ -1,5 +1,37 @@
 package edu.handong.csee.java.hw3;
 
-public class ChatMessageCounter {
 
+import java.io.BufferedReader; 
+import java.io.FileNotFoundException; 
+import java.io.FileReader; 
+import java.io.IOException; 
+import java.util.Scanner; 
+
+
+public class ChatMessageCounter { 
+   public static void main(String[] args) { 
+      Scanner scan = new Scanner(System.in); 
+      System.out.print("filepath : "); 
+      String filePath = scan.nextLine(); 
+      int count = 0; 
+      BufferedReader br = null; 
+      try { 
+         br = new BufferedReader(new FileReader(filePath)); 
+         String line = null; 
+         String[] strArr = null; 
+         int n = 0; 
+         while((line = br.readLine()) != null) { 
+            strArr = line.split(" "); 
+            n = strArr.length; 
+            for(String s:strArr) System.out.println(s); 
+            count += n; 
+         } 
+         } catch (FileNotFoundException fnfe) { 
+         fnfe.printStackTrace(); 
+         } catch (IOException ioe) { 
+         ioe.printStackTrace(); 
+
+         }
+   }
+   
 }
--- src/main/java/edu/handong/csee/java/hw3/DataReader.java
+++ src/main/java/edu/handong/csee/java/hw3/DataReader.java
@@ -1,5 +1,42 @@
 package edu.handong.csee.java.hw3;
 
+import java.io.File;
+import java.util.ArrayList;
+
+//read all chat messages
+
+
 public class DataReader {
+
+	public static void main(String[] args) {
+		DataReader dataReader = new DataReader();
+		dataReader.getData(args[0]);
+	}
+
+	public ArrayList<String> getData(String strDir) {
+
+		getDirectory(strDir);
+		File[] files = getListOfFilesFromDirectory(getDirectory(strDir));
+		ArrayList<String> messages = readFiles(files);
+		return messages;
+	}
+
+	private File getDirectory(String strDir) {		
+		File myDirectory = new File(strDir);
+		return myDirectory;
+	}
+
+	private File[] getListOfFilesFromDirectory(File dataDir) {
+
+		for(File file : dataDir.listFiles()) {
+			System.out.println(file.getAbsolutePath());
+		}
+		return dataDir.listFiles();
+	}
+
+	private ArrayList<String> readFiles(File[] files) {
+		ArrayList<String> messages = new ArrayList<String>();
+		return messages;
+	}
 
 }
